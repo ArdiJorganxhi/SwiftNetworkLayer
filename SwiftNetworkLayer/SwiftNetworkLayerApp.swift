11aeeeed592b46223dabe08a6a08c4eca4c937e1
Ardi Jorganxhi <ardijorganxhiu@gmail.com>
2024-08-13 20:56:20 +0200

    Initial Commit

---
+++ SwiftNetworkLayer/SwiftNetworkLayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNetworkLayerApp.swift
+//  SwiftNetworkLayer
+//
+//  Created by Ardi Jorganxhi on 13/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNetworkLayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
